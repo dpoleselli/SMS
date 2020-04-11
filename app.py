83756abe5c7c50fbadea29e9d23b62a47dfbeca4
dpoleselli <dpoleselli@westmont.edu>
2020-04-11 15:53:26 -0700

    working

--- app.py
+++ app.py
@@ -5,16 +5,12 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
-def test():
-    return Response({'body': 'hello'})
-
-
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our Twilio number
+    # Get the message the user sent to the Twilio number
     body = request.values.get('Body', None)
+
+    body = body.lower()
 
     # Start our TwiML response
     resp = MessagingResponse()
@@ -24,8 +20,11 @@ def incoming_sms():
         resp.message("Hi!")
     elif body == 'bye':
         resp.message("Goodbye")
+    else:
+        resp.message("Que tal mi amigo?")
 
     return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
